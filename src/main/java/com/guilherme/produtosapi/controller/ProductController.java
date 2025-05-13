64c79372b48b19d3f4ede4635ff3dec0d494f5a3
Guilherme <guilhermebarb0sa@proton.me>
2025-05-13 18:34:38 -0300

    Getting products by id

--- src/main/java/com/guilherme/produtosapi/controller/ProductController.java
+++ src/main/java/com/guilherme/produtosapi/controller/ProductController.java
@@ -2,10 +2,7 @@ package com.guilherme.produtosapi.controller;
 
 import com.guilherme.produtosapi.model.Product;
 import com.guilherme.produtosapi.repository.ProductRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -28,5 +25,10 @@ public class ProductController {
 
         productRepository.save(product);
         return product;
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable("id") String id) {
+        return productRepository.findById(id).orElse(null);
     }
 }
