20d08619cd6fa0179b1dde9f17f87f458a4f8651
Guilherme <guilhermebarb0sa@proton.me>
2025-05-13 18:03:48 -0300

    Mapping JPA

--- src/main/java/com/guilherme/produtosapi/model/Product.java
+++ src/main/java/com/guilherme/produtosapi/model/Product.java
@@ -1,11 +1,27 @@
 package com.guilherme.produtosapi.model;
 
-//? POJO -> Plain Old Java Object
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//POJO -> Plain Old Java Object
+@Entity
+@Table(name = "product") //unnecessary if table has the same name as object
 public class Product {
 
+    // annotation Column turns unnecessary if table owns same column names
+    @Id
+    @Column(name = "id")
     private String id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "price")
     private Double price;
 
     public String getId() {
